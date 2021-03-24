dc03715cc885574b8f89c5305ec7205df385d43d
Nikorych Mykola <nt14koo@gmail.com>
2021-03-24 23:02:07 +0200

    Initial commit

---
+++ app/src/main/java/com/cavemanmancave/caveman/ScoreDB.java
@@ -0,0 +1,9 @@
+//This java code is responsible for holding the highscores achieved on each level using data base
+
+package com.cavemanmancave.caveman;
+
+import android.app.ListActivity;
+
+public class ScoreDB extends ListActivity {
+
+}
